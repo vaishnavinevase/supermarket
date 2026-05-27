0c89c2d5bc91f14a71bbf42505eacfe7e76570fd
Ganesh_kute <gkute5633@gmail.com>
2026-05-27 14:07:25 +0530

    New Admin Fetch added

---
+++ Backend/src/main/java/com/supermarket/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.supermarket.backend.repository;
+
+import com.supermarket.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
+}
